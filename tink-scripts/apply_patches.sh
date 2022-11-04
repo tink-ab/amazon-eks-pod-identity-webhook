7c190f4d5da59a5485ccadbe6ae68977aa27300c
Ahmed Magdy <ahmed.magdy@tink.com>
2022-11-04 15:32:28 +0100

    MOON-617: add testing scripts

---
+++ tink-scripts/apply_patches.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git apply tink-patches/0001-Adopt-eks-pod-identity-webhook-for-tink-usecase.patch
---
+++ tink-scripts/revert_patches.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git apply -R tink-patches/0001-Adopt-eks-pod-identity-webhook-for-tink-usecase.patch
